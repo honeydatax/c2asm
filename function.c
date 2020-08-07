a085bad389e4f9ffc00b48d296639531fbcafb9e
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-07 09:14:02 +0100

    function sample test

--- function.c
+++ function.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-int n1=0;
-int n2=0;
+//int n1=0;
+//int n2=0;
 
-int addss();
 
-int addss(){
+
+long addss(n1,n2)
+	long n1,n2;
+{
 	return n1+n2;
 }
 
 int main(){
-	int n=0;
-	n1=100;
-	n2=100;
-	n=addss();
+	long l;
+	long l2;
+	l=100000;
+	l2=200000;
+	l=addss(l,l2);
+	printf("%lu\n",l);
 	return 0;
 }
 
